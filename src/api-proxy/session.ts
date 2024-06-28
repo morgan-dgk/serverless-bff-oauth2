7a14c8d3cc9bf8693a855f8d2703a4d53b0a52f3
morgan-dgk <mdgk95@proton.me>
2024-06-28 12:32:30 +1000

    add docs and use helper to get session info

--- src/api-proxy/session.ts
+++ src/api-proxy/session.ts
@@ -35,4 +35,49 @@ const getSessionInfo = async (sessionId: string): Promise<SessionInfo | undefine
         Key: { sessionId: marshall(sessionId) },
         TableName: 'Sessions',
     };
+
+    const getItemCmd = new GetItemCommand(getItemCommandInput);
+
+    const resp = await dynamodbClient.send(getItemCmd);
+
+    return resp.Item ? (unmarshall(resp.Item) as SessionInfo) : undefined;
+};
+
+/**
+ * Check whether there is an existing session associated with the incoming request
+ *
+ * Event doc: https://docs.aws.amazon.com/apigateway/latest/developerguide/set-up-lambda-proxy-integrations.html#api-gateway-simple-proxy-for-lambda-input-format
+ * @param {Object} event - API Gateway Lambda Proxy Input Format
+ *
+ * Return doc: https://docs.aws.amazon.com/apigateway/latest/developerguide/set-up-lambda-proxy-integrations.html
+ * @returns {Object} object - API Gateway Lambda Proxy Output Format
+ *
+ */
+
+export const lambdaHandler = async (event: APIGatewayProxyEvent): Promise<APIGatewayProxyResult> => {
+    let resp: AuthResponse;
+    console.log(event.headers);
+    const current_time = Math.floor(new Date().getTime() / 1000);
+    const sid = event.headers?.Sid;
+
+    if (!sid) {
+        resp = { isAuthorized: false };
+        return {
+            statusCode: 200,
+            headers: {},
+            body: JSON.stringify(resp),
+        };
+    }
+
+    const sessionInfo = await getSessionInfo(sid);
+
+    if (sessionInfo) {
+        resp = { isAuthorized: true };
+
+        return {
+            statusCode: 200,
+            headers: { headerName: 'headerValue' },
+            body: JSON.stringify(resp),
+        };
+    }
 };
